36e5fde450118010da9454df78f5a32569cd9bd6
adarshhvermaa <103128660+adarshhvermaa@users.noreply.github.com>
2024-08-31 15:12:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const checkStatus = require('./routes/checkStatus');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
